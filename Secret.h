be7492821cc1d13a80f0980717fc0aa36688c5b8
Greekfreaks <149449140+Greekfreaks@users.noreply.github.com>
2024-04-10 16:52:33 +1000

    Add files via upload

---
+++ Secret.h
@@ -0,0 +1,8 @@
+//using bens hotspot
+//my phones hotspot sucks so much :(
+
+#define SECRET_SSID "Ben"
+#define SECRET_PASS "cooldog1"
+
+#define SECRET_CH_ID 2502599            // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "6WWTZ3R8V6VZ7ARQ"   // replace XYZ with your channel write API Key
